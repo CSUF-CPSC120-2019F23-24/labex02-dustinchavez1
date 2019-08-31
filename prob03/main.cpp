1baba9db869cefe2ab2d4792a57e94b01b6dd357
Dustin_chavez <dustin_chavez@cs104u-6f.acad.fullerton.edu>
2019-08-31 14:53:39 -0700

    calculate restaurant total

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,2 +1,22 @@
-// Name: 
+// Name: Dustin Chavez
 // This program calculates the tax and tip on a restaurant bill.
+#include <iostream>
+
+int main()
+{
+  double tax = 1.0775;
+  double tip = 1.20;
+  double grand_total, dollars;
+
+  //  Calculates cost of Restaurant bill
+  std::cout << "What is the cost of the meal? ";
+  std::cin >> dollars;
+  // Cost with tip and tax
+  grand_total = dollars * tax * tip;
+
+  // Display total Cost
+  std::cout << "The \"grand total\" of the meal is "<< grand_total << " dollars\n";
+
+  return 0;
+  
+}
